36645ac54019f165d296b3563776aab6557c7e84
kbys <work4kbys@gmail.com>
2023-04-10 12:10:08 +0900

    script ridy-up

--- make.rb
+++ make.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 require_relative "scripts/utils"
 
-MRUBY = "mruby-3.2.0"
-
 TARGET = ARGV.first
 unless %w(macos-arm64 macos-x86_64 linux mingw).include? TARGET
   puts "make.rb {macos-arm64|macos-x86_64|linux|mingw}"
@@ -11,14 +9,16 @@ end
 PREFIX = "build/#{TARGET}"
 puts "target : #{TARGET}"
 mkdir_p "#{PREFIX}"
-SPRITESHEETER="bismite-sprite-sheeter"
-EXE_NAME=SPRITESHEETER+(TARGET=="mingw"?".exe":"")
+
+EXE_NAME="bismite-sprite-sheeter"+(TARGET=="mingw"?".exe":"")
 
 COMMON_DOWNLOADS = %w(
   https://github.com/bismite/bin_packing/archive/refs/tags/v0.2.0.tar.gz
   bin_packing-0.2.0.tar.gz
+  bin_packing
   https://github.com/mruby/mruby/archive/3.2.0.tar.gz
-  mruby.tgz
+  mruby-3.2.0.tgz
+  mruby
 )
 DOWNLOADS = {
   "linux" => %w(
@@ -53,9 +53,10 @@ def download(url,filename=nil)
 end
 
 Dir.chdir("#{PREFIX}") do
-  COMMON_DOWNLOADS.each_slice(2){|url,filename|
+  COMMON_DOWNLOADS.each_slice(3){|url,filename,dirname|
     download url,filename
-    run "tar xf #{filename}"
+    mkdir_p dirname rescue nil
+    run "tar xf #{filename} -C #{dirname} --strip-component 1"
   }
   DOWNLOADS[TARGET].each{|url|
     download url
@@ -69,67 +70,31 @@ ENV["ARCH"] = "arm64"  if TARGET.end_with?("-arm64")
 ENV["ARCH"] = "x86_64" if TARGET.end_with?("-x86_64")
 ENV["MRUBY_CONFIG"] = File.absolute_path "mruby_config/macos.rb" if TARGET.start_with?("macos")
 rm ENV["MRUBY_CONFIG"]+".lock" rescue nil
-cp "src/mruby-patch.diff", "#{PREFIX}/#{MRUBY}"
-Dir.chdir("#{PREFIX}/#{MRUBY}"){
+cp "src/mruby-patch.diff", "#{PREFIX}/mruby"
+Dir.chdir("#{PREFIX}/mruby"){
   # Patch to mruby
   run "patch -p1 -i mruby-patch.diff"
   run "rake"
 }
 # install mruby
 Dir.chdir(PREFIX){
-  cp_r "#{MRUBY}/include", "./"
-  cp_r "#{MRUBY}/build/#{TARGET}/include", "./"
-  cp_r "#{MRUBY}/build/#{TARGET}/lib", "./"
+  cp_r "mruby/include", "./"
+  cp_r "mruby/build/#{TARGET}/include", "./"
+  cp_r "mruby/build/#{TARGET}/lib", "./"
 }
 
 #
-# compile
+# Compile bismite-sprite-sheeter
 #
-cp "src/sprite-sheeter.c", PREFIX
-cp "src/sprite-sheeter.rb", PREFIX
-cp "scripts/compile.rb", PREFIX
-Dir.chdir(PREFIX){
-  run "./compile.rb sprite-sheeter.rb compiled.rb"
-}
-run "#{PREFIX}/#{MRUBY}/bin/mrbc -B code -o #{PREFIX}/sprite-sheeter.h #{PREFIX}/compiled.rb"
-DEFINES = %w(
-  -DMRB_INT64
-  -DMRB_UTF8_STRING
-  -DMRB_NO_BOXING
-  -DMRB_NO_DEFAULT_RO_DATA_P
-  -DMRB_STR_LENGTH_MAX=0
-).join(" ")
-CFLAGS = %w(-Wall -Werror-implicit-function-declaration -Wwrite-strings -std=gnu11 -O3 -g0).join(" ")
-
-Dir.chdir(PREFIX){
-  INCLUDE = "-Iinclude -Iinclude/SDL2"
-  case TARGET
-  when /macos/
-    arch = ENV["ARCH"]
-    run "clang #{CFLAGS} -arch #{arch} sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmruby lib/libSDL2.a lib/libSDL2_image.a -liconv -lm -framework OpenGL -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-weak_framework,CoreHaptics -Wl,-weak_framework,GameController -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal"
-    run "strip #{EXE_NAME}"
-  when "linux"
-    libs = "lib/libSDL2.a lib/libSDL2_image.a -pthread -lm -lrt"
-    run "clang #{CFLAGS} sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmruby #{libs}"
-    run "strip #{EXE_NAME}"
-  when "mingw"
-    libs = %w(mruby SDL2main SDL2 SDL2_image).map{|l| "lib/lib#{l}.a" }.join(" ")
-    run "x86_64-w64-mingw32-gcc sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmingw32 #{libs} -lopengl32 -lws2_32 -mwindows -Wl,--dynamicbase -Wl,--nxcompat -Wl,--high-entropy-va -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid"
-    run "x86_64-w64-mingw32-strip #{EXE_NAME}"
-  end
-}
+run "./scripts/compile.rb #{TARGET}"
 
 #
-# License
+# Pack
 #
 mkdir_p "#{PREFIX}/licenses/"
 cp "src/licenses.txt", "#{PREFIX}/licenses/"
 rm "#{PREFIX}/licenses/SDL2_mixer-2.6.3-LICENSE.txt"
-
-#
-# Archive
-#
 cp "README.md", PREFIX
 Dir.chdir(PREFIX){
-  run "tar zcf #{SPRITESHEETER}.tgz #{EXE_NAME} README.md licenses/"
+  run "tar zcf bismite-sprite-sheeter.tgz #{EXE_NAME} README.md licenses/"
 }
--- scripts/compile.rb
+++ scripts/compile.rb
@@ -1,152 +1,52 @@
 #!/usr/bin/env ruby
-require "fileutils"
+require_relative "utils"
 
-class Compile
-  attr_reader :included_files, :line_count, :index, :code
-
-  def initialize(mainfile,load_path=[])
-    @mainfile = File.basename(mainfile)
-    @load_path = [ File.dirname(mainfile) ] + load_path
-    puts "load path: #{@load_path.inspect}"
-    @included_files = {}
-    @index = []
-    @line_count = 0
-    @code = ""
-  end
-
-  def run
-    read @mainfile
-    @header =<<EOS
-begin
-EOS
-    @code = @header + @code
-    @code +=<<EOS
-rescue => e
-  _FILE_INDEX_ = #{@index.to_s}
-  table = []
-  _FILE_INDEX_.reverse_each{|i|
-    filename = i[0]
-    start_line = i[1]
-    end_line = i[2]
-    table.fill filename, (start_line..end_line)
-  }
-
-  STDERR.puts "\#{e.class}: \#{e.message}"
-  #STDERR.puts e.backtrace.join("\\n")
-  e.backtrace.each{|b|
-    m = b.chomp.split(":")
-    if m.size < 2
-      puts b
-    else
-      line = m[1].to_i - #{@header.lines.size} -1
-      message = m[2..-1].join(":")
-      original_filename = table[line]
-      original_line = table[0..line].count original_filename
-      STDERR.puts "\#{original_filename}:\#{original_line}:\#{message}"
-    end
-  }
-end
-EOS
-  end
-
-  def write(line)
-    @code << line + "\n"
-    @line_count += 1
-  end
-
-  def memory(file)
-    path = File.expand_path file
-    return false if @included_files[path]
-    @included_files[path] = true
-  end
-
-  def read(filename)
-    filename = filename+".rb" unless filename.end_with? ".rb"
-
-    filepath = nil
-    @load_path.find{|l|
-      f = File.join(l,filename)
-      if File.exists? f
-        filepath = f
-        break
-      end
-    }
-
-    if filepath
-      puts "read #{filepath}"
-    else
-      puts "#{filename} not found"
-      return
-    end
-
-    unless memory filepath
-      puts "#{filepath} already included."
-      return
-    end
-
-    source = File.read(filepath)
-
-    s = source.split "\n"
-    s << "# #{filepath}"
-    start_line = @line_count
-    s.each{|l|
-      if l.start_with? "$LOAD_PATH"
-        write "# #{l}"
-      elsif l.start_with? "require"
-        next_file = l.chomp
-        next_file.slice! "require"
-        next_file.gsub! '"', ''
-        next_file.gsub! "'", ''
-        next_file.gsub! ' ', ''
-        write "# #{l}"
-        self.read next_file
-      else
-        write l
-      end
-    }
-
-    @index << [filename,start_line,@line_count-1]
-  end
-
-  def handle_error_log(error_log)
-    p error_log
-    table = []
-    @index.reverse_each{|i|
-      filename = i[0]
-      start_line = i[1]
-      end_line = i[2]
-      table.fill filename, (start_line..end_line)
-    }
-
-    error_log.each_line{|l|
-      m = l.chomp.split(":")
-      if m.size < 2
-        puts l
-      else
-        line = m[1].to_i - @header.lines.size - 1
-        message = m[2..-1].join(":")
-        original_filename = table[line]
-        if original_filename
-          original_line = table[0..line].count original_filename
-          puts "#{original_filename}:#{original_line}:#{message}"
-        else
-          puts l
-        end
-      end
-    }
-  end
-end
-
-# compile
-INFILE=ARGV[0]
-OUTFILE=ARGV[1]
-compile = Compile.new INFILE, ["bin_packing-0.2.0/lib"]
-
-begin
-  compile.run
-rescue SyntaxError => e
+TARGET = ARGV.first
+unless %w(macos-arm64 macos-x86_64 linux mingw).include? TARGET
+  puts "make.rb {macos-arm64|macos-x86_64|linux|mingw}"
   exit 1
 end
+PREFIX = "build/#{TARGET}"
+EXE_NAME="bismite-sprite-sheeter"+(TARGET=="mingw"?".exe":"")
 
-FileUtils::mkdir_p File.dirname OUTFILE
-File.open(OUTFILE,"wb").write(compile.code)
+#
+# compile .rb
+#
+cp "src/sprite-sheeter.c", PREFIX
+cp "src/sprite-sheeter.rb", PREFIX
+cp "src/merge.rb", PREFIX
+Dir.chdir(PREFIX){
+  run "./merge.rb sprite-sheeter.rb compiled.rb"
+}
+run "#{PREFIX}/mruby/bin/mrbc -B code -o #{PREFIX}/sprite-sheeter.h #{PREFIX}/compiled.rb"
+
+#
+# compile .exe
+#
+DEFINES = %w(
+  -DMRB_INT64
+  -DMRB_UTF8_STRING
+  -DMRB_NO_BOXING
+  -DMRB_NO_DEFAULT_RO_DATA_P
+  -DMRB_STR_LENGTH_MAX=0
+).join(" ")
+CFLAGS = %w(-Wall -Werror-implicit-function-declaration -Wwrite-strings -std=gnu11 -O3 -g0).join(" ")
+
+Dir.chdir(PREFIX){
+  INCLUDE = "-Iinclude -Iinclude/SDL2"
+  case TARGET
+  when /macos/
+    arch = "arm64"  if TARGET.end_with?("-arm64")
+    arch = "x86_64" if TARGET.end_with?("-x86_64")
+    run "clang #{CFLAGS} -arch #{arch} sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmruby lib/libSDL2.a lib/libSDL2_image.a -liconv -lm -framework OpenGL -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-weak_framework,CoreHaptics -Wl,-weak_framework,GameController -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal"
+    run "strip #{EXE_NAME}"
+  when "linux"
+    libs = "lib/libSDL2.a lib/libSDL2_image.a -pthread -lm -lrt"
+    run "clang #{CFLAGS} sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmruby #{libs}"
+    run "strip #{EXE_NAME}"
+  when "mingw"
+    libs = %w(mruby SDL2main SDL2 SDL2_image).map{|l| "lib/lib#{l}.a" }.join(" ")
+    run "x86_64-w64-mingw32-gcc sprite-sheeter.c -o #{EXE_NAME} #{DEFINES} #{INCLUDE} -Llib -lmingw32 #{libs} -lopengl32 -lws2_32 -mwindows -Wl,--dynamicbase -Wl,--nxcompat -Wl,--high-entropy-va -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid"
+    run "x86_64-w64-mingw32-strip #{EXE_NAME}"
+  end
+}
