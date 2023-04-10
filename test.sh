872b5406101bf9fe5fc199081ec1e9a944609e71
kbys <work4kbys@gmail.com>
2023-04-10 11:44:23 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-SPRITESHEETER=$1
+case `uname -s` in
+Linux)
+  TARGET=linux
+  ;;
+Darwin)
+  TARGET=macos-arm64
+  ;;
+esac
 
 mkdir -p build/test
 rm -f build/test/out.png
-${SPRITESHEETER} test/assets build/test
+
+EXE="./build/${TARGET}/bismite-sprite-sheeter test/assets build/test"
+echo "test ${EXE}"
+echo "----"
+${EXE}
+echo "----"
+
 cp test/view.rb build/test
 (cd build/test ; mruby view.rb)
